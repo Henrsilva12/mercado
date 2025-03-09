b15989bf2ee79c40a315bb0996b43a4016488714
Henrique Silva <113844247+Henrsilva12@users.noreply.github.com>
2025-03-09 13:26:21 -0300

    Upgrade: Atualização na routeJs do cliente

--- src/app/api/cliente/route.js
+++ src/app/api/cliente/route.js
@@ -1,16 +1,23 @@
+let users = [];
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
         const { name, endereco, cidade, telefone } = req.body;
 
     if(!name || !endereco || !cidade || !telefone) {
-      res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
+      return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
     }
 
     const newUser = {id: Date.now(), name, endereco, cidade, telefone};
+
+    users.push(newUser);
+
     return res.status(201).json({ message:'Usuário criado com sucesso!', user: newUser});
-    } else {
-        res.setHeader('Allow', ['POST']);
+    } else if (req.method === 'GET') {
+        return res.status(200).json(users);
+    }
+    else {
+        res.setHeader('Allow', ['POST', 'GET']);
         return res.status(405).end( `Method ${req.method} Not Allowed`);
     }
   
